872bf11e6cf7010e3d0de9b3066c970653395549
Matheswaris <119093662+Matheswaris@users.noreply.github.com>
2023-07-12 15:45:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+//  import Login from './Login';
+//  import Signup from './Signup';
+// import Home from './Home'
+// import Aboutus from './Aboutus'
+import Contactus from './Contactus'
+
+function App() {
+  return (
+//  <BrowserRouter>
+//  <Routes>
+//  <Route path="/" element={<Login/>}></Route>
+//  <Route path="/Signup" element={<Signup/>}></Route>
+//  <Route path="/Login" element={<Login/>}></Route>
+//  <Route path="/Home" element={<Home/>}></Route>
+//  </Routes>
+// </BrowserRouter>
+//<div><Home/></div>
+// <div><Aboutus/></div>
+<div><Contactus/></div>
+
+
+  );
+}
+
+export default App;
